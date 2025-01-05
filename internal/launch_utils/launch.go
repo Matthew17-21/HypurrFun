d132c98834842c6702b48cf8a4d12f3db62a3d00
Matthew Lugo <matthewlugo746@gmail.com>
2025-01-05 13:07:12 -0500

    Added method to extend HyperliquidLaunch

--- internal/launch_utils/launch.go
+++ internal/launch_utils/launch.go
@@ -1,3 +1,33 @@
+package launchutils
+
+import (
+	"time"
+
+	"github.com/Matthew17-21/HypurrFun/pb"
+)
+
+// LaunchExtended extends the protobuf HyperliquidLaunch message with additional computed fields
+type LaunchExtended struct {
+	*pb.HyperliquidLaunch
+	LaunchTime time.Time // Time the token was launched
+}
+
+// ToLaunchExtended converts a protobuf HyperliquidLaunch message to an extended launch struct
+// It handles the conversion of the Unix millisecond timestamp to a time.Time object
+func ToLaunchExtended(hl *pb.HyperliquidLaunch) *LaunchExtended {
+	if hl == nil {
+		return &LaunchExtended{
+			HyperliquidLaunch: nil,
+			LaunchTime:        time.Time{}, // Zero value for time.Time
+		}
+	}
+
+	return &LaunchExtended{
+		HyperliquidLaunch: hl,
+		LaunchTime:        unixMilliToTime(hl.ListedTimestamp),
+	}
+}
+
 // UnixMilliToTime converts Unix milliseconds timestamp to time.Time
 func unixMilliToTime(msTimestamp int64) time.Time {
 	return time.UnixMilli(msTimestamp) //.UTC()
